9af966d0ff938fb869aef507e3355a4b3da6dd79
Githerd <158222320+Githerd@users.noreply.github.com>
2026-01-31 23:10:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,15 +9,11 @@ app = Flask(__name__,
 
 @app.route('/')
 def home():
-    return render_template('about.html')
+    return render_template('home.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-@app.route('/articles')
-def articles():
-    return render_template('articles.html')
 
 @app.route('/test')
 def test():
@@ -26,6 +22,22 @@ def test():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/listen')
+def listen():
+    return render_template('main/listen.html', title='Listen')
+
+@app.route('/communicate')
+def communicate():
+    return render_template('main/communicate.html', title='Communicate')
+
+@app.route('/empathise')
+def empathise():
+    return render_template('main/empathise.html', title='Empathise')
+
+@app.route('/resolve')
+def resolve():
+    return render_template('main/resolve.html', title='Resolve')
 
 if __name__ == '__main__':
     app.run(debug=True)
